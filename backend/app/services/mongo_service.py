37d32372a962addbf94b194c1499627c08dbd57f
shubham2605 <shubhamredkar2605@gmail.com>
2026-03-28 09:26:48 +0530

    Created db

---
+++ backend/app/services/mongo_service.py
@@ -0,0 +1,30 @@
+from db.database import collection
+from bson import ObjectId
+
+async def save_scan_result(scan_data: dict):
+    result = await collection.insert_one(scan_data)
+    return str(result.inserted_id)
+
+async def get_scan_result(scan_id: str):
+    result = await collection.find_one({"_id": ObjectId(scan_id)})
+
+    if result:
+        result["_id"] = str(result["_id"])
+
+    return result
+
+async def update_scan_result(scan_id: str, update_data: dict):
+    await collection.update_one(
+        {"_id": ObjectId(scan_id)},
+        {"$set": update_data}
+    )
+
+async def get_all_scans():
+    scans = []
+    cursor = collection.find().sort("_id", -1)
+
+    async for scan in cursor:
+        scan["_id"] = str(scan["_id"])
+        scans.append(scan)
+
+    return scans
